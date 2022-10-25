275453733e2e0a27210d262f753e5213f821b55b
Vinicius Silva <100951497+vingenesi@users.noreply.github.com>
2022-10-25 16:59:32 -0300

    Add files via upload

---
+++ teste login/cadastro.js
@@ -0,0 +1,11 @@
+let user = document.querySelector('#user')
+let senha = document.querySelector('#senha')
+let cornfirmsenha = document.querySelector('#confirmsenha')
+
+user.addEventListener('keyup', () => {
+    
+})
+
+function cadastrar (){
+    
+}
